91b8d26c2af123ac8774cc6f37eac4e3095e480f
Donething <donething.net@gmail.com>
2023-05-17 02:13:42 +0800

    更新库

--- stream/start_test.go
+++ stream/start_test.go
@@ -2,6 +2,7 @@ package stream
 
 import (
 	"github.com/donething/live-dl-go/hanlders"
+	"github.com/donething/live-dl-go/sites/bili"
 	"github.com/donething/live-dl-go/sites/douyin"
 	"github.com/donething/live-dl-go/sites/entity"
 	"github.com/donething/live-dl-go/sites/zuji"
@@ -27,6 +28,19 @@ func TestStartAnchorFlv(t *testing.T) {
 	anchor := entity.Anchor{
 		ID:   "249406961231",
 		Plat: douyin.Plat,
+	}
+
+	err := StartAnchor(capturing, nil, anchor, "D:/Tmp/live/",
+		10*1024*1024, &tgHandler)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestStartAnchorBili(t *testing.T) {
+	anchor := entity.Anchor{
+		ID:   "2011822166",
+		Plat: bili.Plat,
 	}
 
 	err := StartAnchor(capturing, nil, anchor, "D:/Tmp/live/",
