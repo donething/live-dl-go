4e177caf2da4a55290d352b0180d94176b946e94
Donething <donething.net@gmail.com>
2023-12-18 23:54:03 +0800

    增加获取所有键

--- stream/capture_status/capturing.go
+++ stream/capture_status/capturing.go
@@ -45,6 +45,18 @@ func (c *CapStatus[T]) Del(k string) {
 	c.mu.Unlock()
 }
 
+// Keys 返回所有键
+func (c *CapStatus[T]) Keys() []string {
+	keys := make([]string, 0, len(c.cap))
+	c.mu.Lock()
+	for key := range c.cap {
+		keys = append(keys, key)
+	}
+	c.mu.Unlock()
+
+	return keys
+}
+
 // GenCapturingKey 正在录制的主播的键，避免重复录制。格式如 "<平台>_<主播ID>"，如 "bili_12345"
 func GenCapturingKey(anchor *entity.Anchor) string {
 	return fmt.Sprintf("%s_%s", anchor.Plat, anchor.UID)
